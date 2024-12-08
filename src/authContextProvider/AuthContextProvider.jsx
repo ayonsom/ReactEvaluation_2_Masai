46700bcf7b41e9388f31a8d45f29642e6c41eb02
ayonsom <140237905+ayonsom@users.noreply.github.com>
2024-12-08 23:20:16 +0530

    Some minor bugs fixed

--- src/authContextProvider/AuthContextProvider.jsx
+++ src/authContextProvider/AuthContextProvider.jsx
@@ -3,8 +3,12 @@ export const AuthContext = createContext();
 const AuthContextProvider = ({children}) => {
     const [allCountries, setAllCountries] = useState([]);
     const [countryDetails, setCountryDetails] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [inputData, setInputData] = useState("");
   return (
-    <AuthContext.Provider value={{allCountries, setAllCountries, countryDetails, setCountryDetails}}>
+    <AuthContext.Provider value={{allCountries, setAllCountries, 
+        countryDetails, setCountryDetails,
+        loading, setLoading, inputData, setInputData}}>
       {children}
     </AuthContext.Provider>
   )
--- src/components/AllNations.jsx
+++ src/components/AllNations.jsx
@@ -1,25 +1,32 @@
 import React, { useContext } from 'react'
+import { useState } from 'react';
 import { AuthContext } from '../authContextProvider/AuthContextProvider'
 const AllNations = () => {
-    const {allCountries, setAllCountries, countryDetails, setCountryDetails} = useContext(AuthContext);
-    console.log(countryDetails);
+    const {allCountries, setAllCountries, countryDetails, setCountryDetails, loading, setLoading,
+      inputData, setInputData
+    } = useContext(AuthContext);
+
+    const[prev,setPrev] = useState([])
 
     const fetchCountryDetail = async(countryName) =>{
         // console.log(countryName);
         try {
+            setLoading(true);
             const res = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
             const data = await res.json();
             // console.log(data);
-            setCountryDetails(data);
-            
-            
+            setCountryDetails(data);            
         } catch (error) {
-            
+            console.error("Error : ",error)
+        }
+        finally{
+          setLoading(false);
         }
         
     }
 
     const fetchNations = async() =>{
+      // console.log("Clicked")
         try {
             const res = await fetch("https://restcountries.com/v3.1/all");
             const data = await res.json();
@@ -31,32 +38,77 @@ const AllNations = () => {
         }
 
     }
+
+    const handleInput=(e)=>{
+      setInputData(e.target.value)
+    }
+    const findCountry = (e) =>{
+      e.preventDefault();
+      // console.log(inputData);
+      setPrev(allCountries);
+      const arr = allCountries.filter((country)=>(
+        country.name.common.small() === inputData.small()
+      ));
+      // console.log(arr);
+      setAllCountries(arr);
+      
+    }
+
   return (
     <div>
-      <h3>Please click on the button below to fetch all Nations...</h3>
-      <button onClick={fetchNations}>Fetch Nations</button>
-
+      {countryDetails.length === 0 || allCountries.length === 0? (<><h3>Please click on the button below to fetch all Nations...</h3>
+      <button 
+      disabled={loading || allCountries.length !==0 }
+      onClick={fetchNations}
+      
+      >Fetch Nations
+      </button></>) : null}
+      <br />
+      {allCountries.length > 0 && countryDetails.length === 0?
+          (<><input value={inputData} type="text" placeholder='Search a Country Here' onChange={handleInput} style={{padding:"10px"}}/> 
+            <button onClick={findCountry}>Go</button>
+            <button onClick={(e)=>{
+              setInputData("");
+              setAllCountries(prev);}}>
+                Reset
+              </button>
+            </>
+          )
+          : null
+       }
+      <br /><br />
       {countryDetails.length === 0? 
        (<div id="all-countries-container">
+
+        
         {allCountries.map((country, index)=>(
             <div key={index} className="country-card">
                 {/* <p>Count : {index+1}</p> */}
-                <img src={country.flags.png} alt={country.flags.alt} />
-                <h2>Name : {country.name.common}</h2>
-                <h3>Population : {country.population}</h3>
-                <h3>region : {country.region}</h3>
-                <h3>capital : {country.capital}</h3>
+                <div id="flag">
+                  <img id='flag-image' src={country.flags.png} alt={country.flags.alt} />
+                </div>
+                <h3>Name : {country.name.common}</h3>
+                <h4>Population : {country.population}</h4>
+                <h4>region : {country.region}</h4>
+                <h4>capital : {country.capital}</h4>
                 <button onClick={(e)=>fetchCountryDetail(country.name.common)}>More Details</button>
-                <hr />
             </div>
            
         ))}
       </div>) : (
         <div>
             <button onClick={(e)=>setCountryDetails([])}>Back to All Nations</button>
+            <br />
+            <br />
+            <div style={{padding:"20px", background: "#cececec0"}}>
+              <img id='flag-image' src={countryDetails[0]["flags"]["png"]} alt="" />
+            </div>
+            
+            {/* {console.dir(countryDetails[0])} */}
+            {/* <p>{JSON.stringify(countryDetails[0])}</p> */}
             <h2>Country Details : </h2>
-            <h4>Native Name : {countryDetails[0].name.nativeName.eng.official}</h4>
-            <p>Currrency Name : {countryDetails[0].currencies.SHP.name}</p>
+            <h4>Native Name : {countryDetails[0].name.official}</h4>
+            <p>Currrency Name : {JSON.stringify(countryDetails[0].currencies)}</p>
             {/* <p>{JSON.stringify(countryDetails[0])}</p> */}
         
         
